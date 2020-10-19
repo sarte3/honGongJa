1a5945c362b66f9c5891a616e07b848fca6449cc
sarte3 <sarte@outlook.kr>
2020-10-19 13:50:36 +0900

    10/19

---
+++ honGongJa/src/ch14/ProductStorage.java
@@ -0,0 +1,13 @@
+package ch14;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProductStorage {
+
+	private List<Product> list = new ArrayList<Product>();
+	private Scanner scanner=new Scanner(System.in);
+	private int counter;
+	
+}
