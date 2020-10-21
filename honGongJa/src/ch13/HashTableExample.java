c40428104431e36dfa5d3ac9aa5c56b2d4681051
sarte3 <sarte@outlook.kr>
2020-10-21 13:49:09 +0900

    10/21

---
+++ honGongJa/src/ch13/HashTableExample.java
@@ -0,0 +1,16 @@
+package ch13;
+
+import java.util.*;
+
+public class HashTableExample {
+
+	public static void main(String[] args) {
+
+		Map<String, String> map=new Hashtable<String, String>();
+		
+		map.put("spring","12");
+		
+		
+	}
+
+}
