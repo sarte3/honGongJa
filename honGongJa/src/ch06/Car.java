dc3f5309999d9f2cab6d59b11ec16a3a95ae999a
sarte3 <sarte@outlook.kr>
2020-10-26 14:04:25 +0900

    10/26

--- honGongJa/src/ch06/Car.java
+++ honGongJa/src/ch06/Car.java
@@ -3,14 +3,25 @@ package ch06;
 public class Car {
 
 	String company ="현대자동차";
-	String model="그랜저";
-	String color="검정";
+	String model;
+	String color;
 	int maxSpeed=350;
 	int speed;
 	
 	Car(){}
 	
-	Car(String color, int cc){
+	Car(String model){
+		this(model,"은색",250);
+	}
+	
+	Car(String model, String color){
+		this(model, color, 250);
+	}
+	
+	Car(String model, String color, int maxSpeed){
+		this.model=model;
+		this.color=color;
+		this.maxSpeed=maxSpeed;
 		
 	}
 	
---
+++ honGongJa/src/ch07/Child.java
@@ -0,0 +1,17 @@
+package ch07;
+
+public class Child extends Parent{
+
+	public void method2() {
+		
+		System.out.println("Child-method");
+		
+	}
+	
+	public void method3() {
+		
+		System.out.println("Child-method3");
+		
+	}
+	
+}
